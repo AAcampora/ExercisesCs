18b9a5b3683ff8730e9ed6020e1358747dcecfde
Alessio Acampora <acampora199@gmail.com>
2022-07-07 16:09:14 +0100

    lesson about Constructors

---
+++ ConsoleApp1/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        //redundat constructors as they really don't need
+        //initialization
+        //call previous constructor and set Id 
+        public Customer(int id) : this()
+        {
+            this.Id = id;
+        }
+        //call previous constructor and set name
+        public Customer(int id, string name) : this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,29 +2,17 @@
 
 namespace ConsoleApp1
 {
-    public class Person
-    {
-        public string name;
-
-        public void Introduce(string user)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", user, name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.name = str;
-
-            return person;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var p = Person.Parse("Rob");
-            p.Introduce("Alessio");
+            var customer = new Customer(1, "Rob");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
