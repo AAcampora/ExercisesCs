df92eea08e7a99bff648a7c197a439d469577876
Alessio Acampora <acampora199@gmail.com>
2022-07-08 22:24:03 +0100

    added fields exercise

---
+++ Fields/Customer.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Fields
+{
+    public sealed class Customer
+    {
+        public Customer(int id)
+        {
+            ID = id;
+        }
+
+        public Customer(int id, string name) : this(id)
+        {
+            Name = name;
+        }
+
+        public string Name { get;}
+        public int ID { get;}
+
+        public readonly List<Order> Orders = new List<Order>();
+
+        public void Promote()
+        {
+           
+        }
+    }
+}
